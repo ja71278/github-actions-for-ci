1cdf6840f575e1365dfe4f4383738460d402c2e2
ja71278 <85183408+ja71278@users.noreply.github.com>
2021-06-07 12:01:58 -0400

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,7 +2,7 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
+  it('Contains the compiled JavaScript', done => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
@@ -10,6 +10,7 @@ describe('App', () => {
     })
   })
 })
+
 
 describe('Game', () => {
   let game, p1, p2
